bf64a7e91e341358afa86a6d367d572d360a29c1
Rakibul55276 <irakibul550@gmail.com>
2025-11-23 08:22:41 +0300

    m

--- lib/main.dart
+++ lib/main.dart
@@ -1,181 +1,185 @@
-
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatelessWidget{
+
+  const MyApp ({super.key});
 
   @override
   Widget build(BuildContext context) {
-    const String appTitle = 'Flutter layout demo';
-return MaterialApp(
-  title: appTitle,
-  home: Scaffold(
-    backgroundColor: Colors.white,
-    appBar: AppBar(
-      title: const Text(appTitle),
-      centerTitle: true,
-      backgroundColor: Colors.blueGrey[900],
-      titleTextStyle: const TextStyle(color: Colors.white),
-    ),
-    body: SingleChildScrollView(
-      child: Column(
-    children: [
-
-        ImageSection (
-    image: 'images/diamond.png',
-  ),
-  TitleSection(
-    name: 'Oeschinen Lake Campground',
-    location: 'Kandersteg, Switzerland',
-  ),
-  ButtonSection(),
-  TextSection(
-    description:
-        'Lake Oeschinen lies at the foot of the Blüemlisalp in the '
-        'Bernese Alps. Situated 1,578 meters above sea level, it '
-        'is one of the larger Alpine Lakes. A gondola ride from '
-        'Kandersteg, followed by a half-hour walk through pastures '
-        'and pine forest, leads you to the lake, which warms to 20 '
-        'degrees Celsius in the summer. Activities enjoyed here '
-        'include rowing, and riding the summer toboggan run.',
-  ),
-],
-
-      ),
-    ),
-  ),
-);
     
-  }
-}
+    const String apptitle ='flutter demo';
+
+    return MaterialApp(
+
+      title : apptitle,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(apptitle),
+          centerTitle: true,
+          backgroundColor: Colors.blueGrey[900],
+          titleTextStyle: const TextStyle(color: Colors.white),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+
+            children: [
+              ImageSection(
+                image: 'images/diamond.png',
+              ),
+
+              TitleSection(
+
+                name: 'Campaign',
+                location: 'Switzerland',
+              ),
+
+              ButtonSection(),
+
+              TextSection( 
+
+              text:  'he main body of a book or other piece of writing, as distinct from other material such as notes, appendices, and illustrations.',
+              
+              ),
 
 
-
-
-class TitleSection extends StatelessWidget {
-  const TitleSection({super.key, required this.name, required this.location});
-
-  final String name;
-  final String location;
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
-      child: Row(
-        children: [
-          Expanded(
-            /* 1 */
-            child: Column(
-             crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                /* 2 */
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 8),
-                  child: Text(
-                    name,
-                    style: const TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 18,
-                    ),
-                  ),
-                ),
-                Text(
-                  location,
-                  style: TextStyle(
-                    color: Colors.grey[500],
-                    fontSize: 16,
-                  ),
-                ),
-              ],
-            ),
-          ),
-          /* 3 */
-          Icon(Icons.star, color: Colors.red[500]),
-          const SizedBox(width: 4),
-          const Text('41'),
-        ],
-      ),
-    );
-  }
-}
-class ButtonSection extends StatelessWidget {
-  const ButtonSection({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    final Color color = Theme.of(context).primaryColor;
-    return SizedBox(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          ButtonWithText(color: color, icon: Icons.call, label: 'CALL'),
-          ButtonWithText(color: color, icon: Icons.near_me, label: 'ROUTE'),
-          ButtonWithText(color: color, icon: Icons.share, label: 'SHARE'),
-        ],
-      ),
-    );
-  }
-
-}
-
-class ButtonWithText extends StatelessWidget {
-  const ButtonWithText({
-    super.key,
-    required this.color,
-    required this.icon,
-    required this.label,
-  });
-
-  final Color color;
-  final IconData icon;
-  final String label;
-
-  @override
-  Widget build(BuildContext context) {
-  return Column(
-      mainAxisSize: MainAxisSize.min,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Icon(icon, color: color),
-        const SizedBox(height: 8),
-        Text(
-          label,
-          style: TextStyle(
-            fontSize: 12,
-            fontWeight: FontWeight.w400,
-            color: color,
+            ],
           ),
         ),
-      ],
-    );
+      ),
+      );
   }
-
-}
-class TextSection extends StatelessWidget {
-  const TextSection({super.key, required this.description});
-
-  final String description;
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(32),
-      child: Text(description, softWrap: true),
-    );
   }
-}
+      class TitleSection extends StatelessWidget {
+      
+      
+      const TitleSection({super.key, required this.name, required this.location});
+      
+      final String name;
+      final String location;
+      
+        @override
+        Widget build(BuildContext context) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+            child: Row(
+
+              children: [
+
+                Expanded (
+                  child : Column (
+
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+
+                      Padding (
+                        padding : const EdgeInsets.only(bottom:8.0),
+                        child: Text(
+                          name,
+                          style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                        ),
+                      ),
+                      Text(
+                        location,
+                        style: TextStyle(color: Colors.grey[500], fontSize: 10),
+                      ),
+                    ],
+                  ),
+                ),
+
+                Icon (Icons.star, color: Colors.red[500]),
+                const SizedBox(width:4),
+                const Text('41'),
+              ],
+            ),
+          );
+        }
+      
+      }
+
+      class ButtonSection extends StatelessWidget {
+
+        const ButtonSection ({super.key});
+
+        @override
+        Widget build (BuildContext context) {
+          final Color color = Theme.of(context).primaryColor;
+
+          return SizedBox(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ButtonWithText(color: color, icon: Icons.call, label: 'CALL'),
+                ButtonWithText(color: color, icon: Icons.near_me, label: 'ROUTE'),
+                ButtonWithText(color: color, icon: Icons.share, label: 'SHARE'),
+              ],
+            ),
+          );
+        }
+      }
+
+      class ButtonWithText extends StatelessWidget {
+        const ButtonWithText({
+          super.key,
+          required this.color,
+          required this.icon,
+          required this.label,
+        });
+
+        final Color color;
+        final IconData icon;
+        final String label;
+
+        @override
+        Widget build(BuildContext context) {
+          return Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(icon, color: color),
+              const SizedBox(height: 8),
+              Text(
+                label,
+                style: TextStyle(fontSize: 12, color: color, fontWeight: FontWeight.bold),
+              ),
+            ],
+          );
+        }
+      }
 
 
-class ImageSection extends StatelessWidget {
-  const ImageSection({super.key, required this.image});
 
-  final String image;
+      class TextSection extends StatelessWidget {
+        const TextSection({super.key, required this.text});
 
-  @override
-  Widget build(BuildContext context) {
-    return Image.asset(image, width: 600, height: 240, fit: BoxFit.cover);
-  }
-}
+        final String text;
+
+        @override
+        Widget build(BuildContext context) {
+          return Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Text(
+              text,
+              softWrap: true,
+              style: const TextStyle(fontSize: 14),
+            ),
+          );
+        }
+      }
+
+      class ImageSection extends StatelessWidget {
+        const ImageSection({super.key, required this.image});
+
+        final String image;
+
+        @override
+        Widget build(BuildContext context) {
+          return Image.asset(
+            image,
+            width: double.infinity,
+            height: 240,
+            fit: BoxFit.cover,
+          );
+        }
+      }
