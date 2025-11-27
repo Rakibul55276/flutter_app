2b43e9de17b10f2d8c98b1fd5876ef5f09c4efc2
Rakibul55276 <irakibul550@gmail.com>
2025-11-27 12:13:19 +0300

    init

--- lib/main.dart
+++ lib/main.dart
@@ -1,141 +1,25 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Text('Dicee'),
+          backgroundColor: Colors.red,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class DicePage extends StatelessWidget {
+  const DicePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              CircleAvatar(
-                radius: 50.0,
-                backgroundImage: AssetImage('images/diamond.png'),
-              ),
-              Text(
-                'Rakib Yu',
-                style: TextStyle(
-                  fontFamily: 'Pacifico',
-                  fontSize: 40.0,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              Text(
-                'FLUTTER DEVELOPER',
-                style: TextStyle(
-                  fontFamily: 'SourceCodePro',
-                  fontSize: 20.0,
-                  color: Colors.teal.shade100,
-                  letterSpacing: 2.5,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-
-              SizedBox(
-                height: 20.0,
-                width: 150.0,
-                child: Divider(
-                  color: Colors.teal.shade100,
-                ),),
-              Card(
-                  color: Colors.white,
-                  //padding: EdgeInsets.all(10.0),
-                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                 child: ListTile(
-                    leading: Icon(
-                      Icons.phone,
-                      color: Colors.teal,
-                    ),
-                    title: Text('+584985734579735',
-                    style: TextStyle(color: Colors.teal.shade900
-                    ,fontFamily: 'SourceCodePro',fontSize: 20.0),
-                      
-                    ),
-                 )
-               
-            
-             ),
-
-
-
-             Card(
-              color: Colors.white,
-              //padding: EdgeInsets.all(10.0),
-              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-
-              child: ListTile(
-                leading: Icon(
-                  Icons.email,
-                  color: Colors.teal,
-                ),
-                title: Text('rakibyu@gmail.com',style: TextStyle(color: Colors.teal.shade900
-                  ,fontFamily: 'SourceCodePro',fontSize: 20.0),
-              ),
-              ),
-             
-              
-              
-
-
-              )
-             
-            ],
-          ),
-        ),
-      ),
-    );
+    return Container();
   }
 }
-
-
-// class MyHomePage extends StatefulWidget {
-//   const MyHomePage({super.key});
-
-//   @override
-//   // ignore: library_private_types_in_public_api
-//   _MyHomePageState createState() => _MyHomePageState();
-// }
-
-// class _MyHomePageState extends State<MyHomePage> {
-//   int nDonuts = 0;
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         backgroundColor: Colors.red,
-//         title: const Text('My Flutter App'),
-//         centerTitle: true,
-//         titleTextStyle: const TextStyle(
-//           fontSize: 24,
-//           color: Colors.white,
-//       ),
-//       ),
-//       body: Center(
-//         child: Text(
-//           'Number of donuts eaten: $nDonuts',
-//           style: const TextStyle(fontSize: 22),
-//         ),
-//       ),
-//       floatingActionButton: FloatingActionButton(
-//         backgroundColor: Colors.red,
-//         child: const Icon(Icons.add),
-//         onPressed: () {
-//           setState(() {
-//             nDonuts++;
-//           });
-//         },
-//       ),
-//     );
-//   }
-// }
