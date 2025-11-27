5fead76c0f8bd2f659a9672f1541bf270403b9d8
Rakibul55276 <irakibul550@gmail.com>
2025-11-27 10:55:46 +0300

    card module done

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
-            //mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               CircleAvatar(
                 radius: 50.0,
@@ -40,6 +40,54 @@ class MyApp extends StatelessWidget {
                   fontWeight: FontWeight.bold,
                 ),
               ),
+
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.teal.shade100,
+                ),),
+              Card(
+                  color: Colors.white,
+                  //padding: EdgeInsets.all(10.0),
+                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                 child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                    ),
+                    title: Text('+584985734579735',
+                    style: TextStyle(color: Colors.teal.shade900
+                    ,fontFamily: 'SourceCodePro',fontSize: 20.0),
+                      
+                    ),
+                 )
+               
+            
+             ),
+
+
+
+             Card(
+              color: Colors.white,
+              //padding: EdgeInsets.all(10.0),
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+
+              child: ListTile(
+                leading: Icon(
+                  Icons.email,
+                  color: Colors.teal,
+                ),
+                title: Text('rakibyu@gmail.com',style: TextStyle(color: Colors.teal.shade900
+                  ,fontFamily: 'SourceCodePro',fontSize: 20.0),
+              ),
+              ),
+             
+              
+              
+
+
+              )
              
             ],
           ),
