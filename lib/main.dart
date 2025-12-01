1ac347b5e583227e4cde4c746440fe7923576ef1
Rakibul55276 <irakibul550@gmail.com>
2025-12-01 12:30:51 +0300

    Random Magic Ball

--- lib/main.dart
+++ lib/main.dart
@@ -1,89 +1,44 @@
 import 'package:flutter/material.dart';
 import 'dart:math';
-void main() {
-  return runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.red,
-        appBar: AppBar(
-       backgroundColor: Colors.red.shade900,
-          title: Text('Dicee', style: TextStyle(color: Colors.white)),
-          centerTitle: true,
-        ),
-        body: DicePage(),
+
+void main() => runApp(
+      MaterialApp(
+        home: BallPage(),
       ),
-    ),
-  );
-}
-class DicePage extends StatefulWidget {
-  const DicePage({super.key});
+    );
 
-  
+class BallPage extends StatefulWidget {
+  const BallPage({super.key});
+
   @override
-  _DicePageState  createState() => _DicePageState();
+  _BallPageState createState() => _BallPageState();
 }
 
-class _DicePageState extends State<DicePage> {
-  int leftDiceNumber = 1;
-int rightDiceNumber = 1;
+class _BallPageState extends State<BallPage> {
+  int ballNumber = 1;
 
+  void changeBall() {
+    setState(() {
+      ballNumber = Random().nextInt(5) + 1; // values 1–5
+    });
+  }
 
-void changeDiceFace(){
-
-  setState(() {
-     rightDiceNumber = Random().nextInt(6) + 1;
-  leftDiceNumber = Random().nextInt(6) + 1;
-  });
-
-
-}
   @override
   Widget build(BuildContext context) {
-    return Column(
-    
-      children: [
-
-
-
-        Expanded(
-          child: Center(
-            child: Row(
-             mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                TextButton(
-                  onPressed: () {
-                    setState(() {
-                      changeDiceFace();
-                      debugPrint('Left Dice Pressed $leftDiceNumber');
-                    });
-                  },
-                  child: Image.asset(
-                    'images/dice$leftDiceNumber.png',
-                    width: 120,
-                  ),
-                ),
-            
-                SizedBox(width: 20), // space between dice
-            
-                TextButton(
-                  onPressed: () {
-                 changeDiceFace();
-                 debugPrint('Right Dice Pressed $rightDiceNumber');
-                  },
-                  child: Image.asset(
-                    'images/dice$rightDiceNumber.png',
-                    width: 120,
-                  ),
-                ),
-              ],
-            ),
-          ),
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        title: const Text('Ask Me Anything'),
+        backgroundColor: Colors.blue.shade900,
+      ),
+      body: Center(
+        child: TextButton(
+          onPressed: () {
+            changeBall();
+          },
+          child: Image.asset('images/ball$ballNumber.png'),
         ),
-      ],
+      ),
     );
   }
-  }
-
-
-
-
+}
